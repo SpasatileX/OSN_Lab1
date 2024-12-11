85a7be4dc39214d9c479c306aa14851bc91297b1
SpasatileX <73082572+SpasatileX@users.noreply.github.com>
2024-12-11 22:51:09 +0200

    fc

---
+++ DiseaseData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1;
+
+class DiseaseData
+{
+    [LoadColumn(0)] public float Age;
+    [LoadColumn(1)] public float NumberOfSexualPartners;
+    [LoadColumn(2)] public float FirstSexualIntercourse;
+    [LoadColumn(3)] public float NumOfPregnancies;
+    [LoadColumn(4)] public float Smokes;
+    [LoadColumn(5)] public float SmokesYears;
+    [LoadColumn(6)] public float SmokesPacksPerYear;
+    [LoadColumn(7)] public float HormonalContraceptives;
+    [LoadColumn(8)] public float HormonalContraceptivesYears;
+    [LoadColumn(9)] public float IUD;
+    [LoadColumn(10)] public float IUDYears;
+    [LoadColumn(11)] public float STDs;
+    [LoadColumn(12)] public float STDsNumber;
+    [LoadColumn(13)] public float STDsCondylomatosis;
+    [LoadColumn(14)] public float STDscervicalCondylomatosis;
+    [LoadColumn(15)] public float STDsvaginalCondylomatosis;
+    [LoadColumn(16)] public float STDsvulvoPerinealCondylomatosis;
+    [LoadColumn(17)] public float STDssyphilis;
+    [LoadColumn(18)] public float STDspelvicInflammatoryDisease;
+    [LoadColumn(19)] public float STDsgenitalHerpes;
+    [LoadColumn(20)] public float STDsmolluscumContagiosum;
+    [LoadColumn(21)] public float STDsAIDS;
+    [LoadColumn(22)] public float STDsHIV;
+    [LoadColumn(23)] public float STDsHepatitisB;
+    [LoadColumn(24)] public float STDsHPV;
+    [LoadColumn(25)] public float STDsNumberOfDiagnosis;
+    [LoadColumn(26)] public float STDsTimeSinceFirstDiagnosis;
+    [LoadColumn(27)] public float STDsTimeSinceLastDiagnosis;
+    [LoadColumn(28)] public float DxCancer;
+    [LoadColumn(29)] public float DxCIN;
+    [LoadColumn(30)] public float DxHPV;
+    [LoadColumn(31)] public bool Label;
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+namespace Lab1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string dataPath = "../../../data.csv";
+
+        var mlContext = new MLContext();
+
+        var data = mlContext.Data.LoadFromTextFile<DiseaseData>(
+            dataPath,
+            hasHeader: true,
+            separatorChar: ',');
+
+        var dataSplit = mlContext.Data.TrainTestSplit(data, testFraction: 0.2);
+        var trainingData = dataSplit.TrainSet;
+        var testData = dataSplit.TestSet;
+
+        var pipeline = mlContext.Transforms.Concatenate("Features",
+                nameof(DiseaseData.Age),
+                nameof(DiseaseData.NumberOfSexualPartners),
+                nameof(DiseaseData.FirstSexualIntercourse),
+                nameof(DiseaseData.NumOfPregnancies),
+                nameof(DiseaseData.Smokes),
+                nameof(DiseaseData.SmokesYears),
+                nameof(DiseaseData.SmokesPacksPerYear),
+                nameof(DiseaseData.HormonalContraceptives),
+                nameof(DiseaseData.HormonalContraceptivesYears),
+                nameof(DiseaseData.IUD),
+                nameof(DiseaseData.IUDYears))
+            .Append(mlContext.Transforms.ReplaceMissingValues("Features", replacementMode: MissingValueReplacingEstimator.ReplacementMode.Mean))
+            .Append(mlContext.Transforms.NormalizeMinMax("Features"))
+            .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(new Microsoft.ML.Trainers.SdcaLogisticRegressionBinaryTrainer.Options
+            {
+                LabelColumnName = "Label",
+                FeatureColumnName = "Features"
+            }));
+
+        var model = pipeline.Fit(trainingData);
+
+        var predictions = model.Transform(testData);
+        var metrics = mlContext.BinaryClassification.Evaluate(predictions, labelColumnName: "Label");
+
+        Console.WriteLine($"Accuracy: {metrics.Accuracy}");
+        Console.WriteLine($"F1 Score: {metrics.F1Score}");
+        Console.WriteLine($"AUC: {metrics.AreaUnderRocCurve}");
+
+        string modelPath = "output.zip";
+        mlContext.Model.Save(model, trainingData.Schema, modelPath);
+        Console.WriteLine($"Модель збережено у {modelPath}");
+    }
+}
+
